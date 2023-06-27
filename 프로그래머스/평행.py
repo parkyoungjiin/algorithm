4713d3bb9ac68eb95a6a52df4db750a868b2e8e6
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-06-27 23:41:42 +0900

    2023-06-27 기본문제 풀이

---
+++ 프로그래머스/평행.py
@@ -0,0 +1,24 @@
+"""
+점 네 개의 좌표를 담은 이차원 배열  dots가 다음과 같이 매개변수로 주어집니다.
+
+[[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
+주어진 네 개의 점을 두 개씩 이었을 때, 두 직선이 평행이 되는 경우가 있으면 1을 없으면 0을 return 하도록 solution 함수를 완성해보세요.
+
+
+"""
+
+def solution(dots):
+    answer = 0
+    # 변수 선언
+    if slope(dots[0],dots[1]) == slope(dots[2],dots[3]):
+        answer = 1
+    if slope(dots[0],dots[2]) == slope(dots[1],dots[3]):
+        answer = 1
+    if slope(dots[0],dots[3]) == slope(dots[1],dots[2]):
+        answer = 1
+    
+    return answer
+# 기울기 구하기
+def slope(dot1, dot2):
+    return (dot1[1]-dot2[1]) / (dot1[0]-dot2[0])
+solution([[1, 4], [9, 2], [3, 8], [11, 6]])

e5952e46b58a54b9ba2d13e150cac8c2800b2dd7
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-07-15 23:48:08 +0900

    2023-07-15 Lv1.

--- 프로그래머스/Lv1/추억 점수.py
+++ 프로그래머스/Lv1/추억 점수.py
@@ -33,6 +33,6 @@ def solution(name, yearning, photo):
     
     # print(name_dict)
     # print(yearning_dict)
-    return print(answer)
+    return answer
 
 solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])

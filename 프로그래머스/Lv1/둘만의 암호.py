4db7ca0fb2c73379e5c95a73fabf5a2a3254f449
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-07-22 23:54:40 +0900

    2023-07-22 LV1. 문제

--- 프로그래머스/Lv1/둘만의 암호.py
+++ 프로그래머스/Lv1/둘만의 암호.py
@@ -1,14 +1,14 @@
 def solution(s, skip, index):
-    answer = ""
+    # skip은 제외할 인덱스번호, 
+    answer = []
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    for i in skip:
+        if i in alpha:
+            alpha = alpha.replace("", i)
     
-    alpha = "abcdefghijklmnopqrstuvwxyz" # 알파벳
+    print(alpha)
+
     
-    for ch in skip: # ch => skip의 문자 하나하나
-        if ch in alpha:
-            alpha = alpha.replace(ch, "") # 알파벳 안에 skip 문자들 제거
-    
-    for i in s:
-        change = alpha[(alpha.index(i) + index) % len(alpha)] # s의 문자 인덱스 + index를 alpha의 길이로 나눈 나머지를 알파벳으로 변환
-        answer += change
-    
+        
     return answer
+solution("aukksasdew","wbqd",5)
--- 프로그래머스/Lv1/카드뭉치.py
+++ 프로그래머스/Lv1/카드뭉치.py
@@ -2,32 +2,18 @@ from collections import deque
 
 
 def solution(cards1, cards2, goal):
-    cards1 = deque(cards1)
-    cards2 = deque(cards2)
-
+    cards1, cards2 = deque(cards1), deque(cards2)
     for i in goal:
-        if cards1:
-            if i == cards1[0]:
-                cards1.popleft()
-            else:
-                continue
-    for i in goal:
-        if cards2:
-            if i == cards2[0]:
-                cards2.popleft()
-            else:
-                continue
-   
-        
-    if len(cards1) == 0 and len(cards2) == 0:
-        answer = "Yes"
-    else:
-        answer = "No"
-
-    return print(answer)
-        
+        if len(cards1) and i == cards1[0]:
+            cards1.popleft()
+        elif len(cards2) and i == cards2[0]:
+            cards2.popleft()
+        else:
+            return 'No'     
+    return 'Yes'
 
 
-solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])
-solution(["i", "water", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"])
-solution(["def", "bcd"], ["b", "c", "d"], ["bcd"])
+# solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])
+solution(["i", "drink", "water"], ["drink", "water"],  ["i", "drink", "water"])
+# solution(["i", "water", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"])
+# solution(["def", "bcd"], ["b", "c", "d"], ["bcd"])

b4ee09fb9627374ef1606e6b28e27ec622f19269
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-08-03 23:38:58 +0900

    2023-08-03 Lv1. 기본문제

--- 프로그래머스/Lv1/가장 가까운 같은 글자.py
+++ 프로그래머스/Lv1/가장 가까운 같은 글자.py
@@ -1,8 +1,17 @@
 def solution(s):
     answer = []
-    for i in range(len(s)):
-        if s[i] in s[0:i]:
-            answer.append(i - s[0:i].rindex(s[i]))
-        else:
+    # 맨 처음 나오면 -1, 아닐 경우는 몇 번쨰 앞에 잇는 지 리턴
+    word_dict = {}
+    for idx, word in enumerate(s):
+        # print(idx, word)
+        if word not in word_dict:
             answer.append(-1)
+            word_dict[word] = idx
+        else:
+            answer.append(idx - word_dict[word])
+            word_dict[word] = idx
+
+
     return answer
+
+solution("banana")

b100f01700a4d11a61682bccfc19981f674e1fe5
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-08-30 17:40:07 +0900

    2023-08-30 Lv1. 문제

---
+++ 프로그래머스/Lv1/숫자 문자열과 영단어.py
@@ -0,0 +1,17 @@
+
+import re
+
+
+def solution(s):
+    answer = 0
+    alpha_dict = {"zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+
+    for key in alpha_dict.keys():
+        if key in s:
+            s = s.replace(key, str(alpha_dict[key]))
+    
+
+    return int(s)
+
+solution("one4seveneight")
+solution("23four5six7")

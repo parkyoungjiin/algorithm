3e5517351f49669cff3a33e1064cec22aec4f039
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-09-14 23:44:44 +0900

    2023-09-14 Lv1. 문제

---
+++ 프로그래머스/Lv1/다트 게임.py
@@ -0,0 +1,23 @@
+def solution(dartResult):
+    stack = []
+    dartResult = dartResult.replace("10", "A")
+    bonus = {'S': 1, 'D': 2, 'T': 3}
+    
+    for i in dartResult:
+        if i.isdigit() or i=='A':
+            stack.append(10 if i == 'A' else int(i))
+        elif i in ('S', 'D', 'T'):
+            num = stack.pop()
+            stack.append(num ** bonus[i])
+        elif i == '#':
+            stack[-1] *= -1
+        elif i == '*':
+            num = stack.pop()
+            if len(stack):
+                stack[-1] *= 2
+            stack.append(2 * num)
+    return sum(stack)
+
+solution("1S2D*3T")
+solution("1D2S#10S")
+solution("1D2S0T")
--- 프로그래머스/Lv1/예산.py
+++ 프로그래머스/Lv1/예산.py
@@ -1,11 +1,16 @@
 def solution(d, budget):
     answer = 0
-    
+    # 예산에서 d에 있는 원소들을 하나씩 빼면서 answer 
+    # 1. 기존 예산보다 작을 때만 작업 진행. (정렬 후 작은 값부터 빼주면 될듯)
+    # 2. 
     d = sorted(d)
     
-    for i in range(len(d)) :
-        if budget >= d[i] :
-            budget -= d[i]
+    for i in d:
+        if budget >= i:
+            budget -= i
             answer += 1
-    
+
     return answer
+
+solution([1,3,2,5,4], 9)
+solution([2,2,3,3], 10)

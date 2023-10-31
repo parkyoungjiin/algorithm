5e729e917cf7f658db08ad79adf8e57df5dcf43f
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-10-31 23:45:48 +0900

    2023-10-31 스터디 문제
    그리디 문제 풀이

--- 백준/그리디/세탁소 사장 동혁.py
+++ 백준/그리디/세탁소 사장 동혁.py
@@ -2,15 +2,17 @@
 test_case = int(input())
 
 # 잔돈 유형
-change_type = [0.25, 0.10, 0.05, 0.01]
+change_type = [25, 10, 5, 1]
 # 테스트 케이스 개수만큼 입력받기.
 for i in range(test_case):
     # 잔돈 개수
     change_num = []
     # 잔돈
-    change = int(input()) / 100
+    change = int(input())
+    
     for c in change_type:
-        change_num.append(int(change // c))
-        change %= c
+        print(change)
+        change_num.append(change // c)
+        change = change % c
     # 언패킹(*) : 패킹된 변수를 하나씩 풀어준다.
     print(*change_num)

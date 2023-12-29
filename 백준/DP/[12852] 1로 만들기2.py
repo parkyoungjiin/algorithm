0f636cf5a1612dc8fe953716a5163ca21e0c17c1
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-29 09:54:14 +0900

    2023-12-29 스터디 문제
    2차원 배열을 사용하여 다시 풀어보았음.

--- 백준/DP/[12852] 1로 만들기2.py
+++ 백준/DP/[12852] 1로 만들기2.py
@@ -2,30 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-# dp = 1로 만드는 최소 횟수
-dp = [0] * (n+1)
-answer = []
-if n >= 2:
-    for i in range(2, n+1):
-        # 1로 뺴는 연산을 먼저 수행.
-        dp[i] = dp[i-1] + 1
-        # 만약 2로 나눠지거나, 3으로 나눠지는 경우가 더 연산 횟수가 적을 수 있기에 min 수행.
-        if i % 2 == 0:
-            dp[i] = min(dp[i], dp[i//2] + 1)
-        if i % 3 == 0:
-            dp[i] = min(dp[i], dp[i//3] + 1)
-print(dp[n])
+# dp = [[연산횟수, [1로 되는 숫자들]]]
+dp = [[0, []] for _ in range(n+1)]
+dp[1][0] = 0
+dp[1][1] = [1]
 
-result = [n]
-now = n
-# temp => n에서 -1한 값의 dp(최소 연산)
-temp = dp[n] -1
-# 1씩 내려가면서 순서 찾기.
-for i in range(n, 0, -1):
-    if dp[i] == temp and (i+1 == now or i*2 == now or i*3 == now):
-        now = i
-        result.append(i)
-        temp -= 1
-print(result)
+
+for i in range(2, n+1):
+    dp[i][0] = dp[i-1][0] + 1
+    # 리스트 + 리스트 -> 두 리스트를 합쳐서 새로운 리스트를 반환하기에 두 리스트는 변경되지 않는다.
+    # append는 현재 리스트에 새로운 요소를 추가하는 것이기에 현재 리스트가 변경된다.
+    dp[i][1] = dp[i-1][1] + [i]
+
+    if i % 3 == 0 and dp[i//3][0] + 1 < dp[i][0] :
+        dp[i][0] = dp[i//3][0] + 1
+        dp[i][1] = dp[i//3][1] + [i]
+    
+    if i % 2 == 0 and dp[i//2][0] + 1 < dp[i][0] :
+        dp[i][0] = dp[i//2][0] + 1
+        dp[i][1] = dp[i//2][1] + [i]
+    
+print(dp)
+
 
 

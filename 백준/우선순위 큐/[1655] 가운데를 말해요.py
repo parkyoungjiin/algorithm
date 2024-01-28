f8766232abdcde657533015f896ae86cef28f8d5
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-01-28 23:54:14 +0900

    2024-01-28 우선순위 큐 복습

--- 백준/우선순위 큐/[1655] 가운데를 말해요.py
+++ 백준/우선순위 큐/[1655] 가운데를 말해요.py
@@ -1,14 +1,23 @@
-import sys
 import heapq
+import sys
 
-input = sys.stdin.readline
+n = int(sys.stdin.readline())
 
-n = int(input())
+leftHeap = []
+rightHeap = []
+for i in range(n):
+    num = int(sys.stdin.readline())
 
-num_list = []
-for _ in range(n):
-    heapq.heappush(num_list,int(input()))
-    num_list.sort() 
-    print(num_list)
+    if len(leftHeap) == len(rightHeap):
+        heapq.heappush(leftHeap, -num)
+    else:
+        heapq.heappush(rightHeap, num)
 
+    if rightHeap and rightHeap[0] < -leftHeap[0]:
+        leftValue = heapq.heappop(leftHeap)
+        rightValue = heapq.heappop(rightHeap)
 
+        heapq.heappush(leftHeap, -rightValue)
+        heapq.heappush(rightHeap, -leftValue)
+
+    print(-leftHeap[0])

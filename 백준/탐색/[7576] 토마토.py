2206a9f5cf71b9c42a9791d92bcc129e64b0b534
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-02-14 22:23:26 +0900

    2024-02-14 탐색 복습
    로봇 청소기를 풀지 못함...

--- 백준/탐색/[7576] 토마토.py
+++ 백준/탐색/[7576] 토마토.py
@@ -1,38 +1,45 @@
 from collections import deque
-M, N = map(int,input().split())
+import sys
 
-graph = [list(map(int, input().split())) for _ in range(N)]
+input = sys.stdin.readline
+# 행, 열
+m, n  = map(int, input().split())
+tomato = []
+for _ in range(n):
+    tomato.append(list(map(int,input().split())))
+# tomato = [list(map(int,input().split())) for _ in range(n)]
 
-print(graph)
-start, end = 0, 0
-# 상하좌우
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-queue = deque()
-for i in range(N):
-    for j in range(M):
-        # print(j, i)
-        # print(graph[j][i])
-        if graph[i][j] == 1:
-            queue.append((i,j))
-print(queue)
-    
-while queue:
-    s, e = queue.popleft()
+# 좌표
+dr = [-1, 0, 1, 0]
+dc = [0, -1, 0, 1]
+# 결과
+res = 0
+q = deque([])
+
+for i in range(n):
+    for j in range(m):
+        # 익은 토마토 인 경우
+        if tomato[i][j] == 1:
+            q.append([i,j])
+# bfs 탐색
+while q:
+    r, c = q.popleft()
+    # 인접한 토마토 익음
     for i in range(4):
-        nx = s + dx[i]
-        ny = e + dy[i]
-        # print(nx, ny)
-        if 0<=nx<N and 0<=ny<M:
-            if graph[nx][ny] == 0:
-                queue.append([nx,ny])
-                graph[nx][ny] = graph[s][e] + 1
-
-        
-print(graph)
-# 2차원 행렬에서 최대값 원소 찾기
-print(max(map(max,graph))-1)
-
-
-
+        nr = r + dr[i]
+        nc = c + dc[i]
+        if 0 <= nr < n and 0 <= nc < m:
+            if tomato[nr][nc] == 0:
+                tomato[nr][nc] = tomato[r][c] + 1
+                q.append((nr,nc))
+# 토마토를 다 탐색하면서 0이 있다면 익지 않은 것이기에 -1를 출력하고 종료.
+for t_list in tomato:
+    for t in t_list:
+        if t == 0:
+            print(-1)
+            exit()
+    # 0 이 없는 경우 max 값 갱신
+    res = max(res, max(t_list))
+# 1로 시작하기에 1을 빼준다.(날짜)
+print(res-1)
 

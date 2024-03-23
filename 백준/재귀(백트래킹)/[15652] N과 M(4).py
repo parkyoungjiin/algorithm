a09c455c7286e88edf315977d01743506b4545f3
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-03-23 23:40:10 +0900

    2024-03-23 문제 복습

--- 백준/재귀(백트래킹)/[15652] N과 M(4).py
+++ 백준/재귀(백트래킹)/[15652] N과 M(4).py
@@ -1,4 +1,15 @@
 n,m = map(int, input().split())
-
-for i in range(1, n):
+ 
+s = []
+ 
+def dfs(start):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
     
+    for i in range(start, n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+    
+dfs(1)

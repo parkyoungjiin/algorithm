2214bafef23633db008ccbd3c5d7e9889002187d
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-03-24 23:57:36 +0900

    2024-03-24 문제 복습

--- 백준/정렬/[1629] 곱셈.py
+++ 백준/정렬/[1629] 곱셈.py
@@ -1,14 +1,10 @@
-
-def dac(a, b, c):
-    if b == 0:
+def solution(a, b, c):
+    if b == 1:
         return a % c
-    if b % 2 == 0:
-        return (dac(a,b//2,c) ** 2) %c
-    else:
-        return ()
-a, b, c = map(int, input().split())
-result = 1
-for i in range(b):
-    result *= a
 
-print(result%c)
+    temp = solution(a, b // 2, c)
+
+    if b % 2 == 1:
+        return ((temp * temp) % c) * a % c
+    else:
+        return (temp * temp) % c

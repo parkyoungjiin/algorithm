c4673de2dcd5379f1f99e6d80f7e77917b6ee556
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-06-20 23:20:28 +0900

    2024-06-20 복습

--- 백준/DP/[1495] 기타리스트.py
+++ 백준/DP/[1495] 기타리스트.py
@@ -1,27 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-n, s, max_vol = map(int, input().split())
+n, s, m = map(int, input().split())
 
-volume = list(map(int, input().split()))
+vol = list(map(int, input().split()))
 
-dp = [[0] * (max_vol+1) for _ in range(n+1)] # 곡 개수(n)만큼 최대 볼륨값 길이의 배열을 만든다.
-
-dp[0][s] = 1 # 시작 볼륨을 첫 번째 곡의 볼륨에 1을 넣는다. 
+ans = [[0] * (m+1) for i in range(n)]
 
 for i in range(n):
-    for j in range(max_vol+1): 
-        if dp[i][j] == 1: 
-            min_val = j - volume[i]
-            max_val = j + volume[i]
-            if min_val >= 0:
-                dp[i+1][min_val] = 1
+    min_vol = s - vol[i]
+    max_vol = s + vol[i]
 
-            if max_val <= max_vol:
-                dp[i+1][max_val] = 1
-result = -1
-for i in range(max_vol, -1, -1): # (max_vol ~ -1) -1하면서 진행
-    if dp[n][i] == 1: # 마지막 곡의 최대 볼륨 수 출력을 위해 [n][i]
-        result = i
-        break
-print(result)
+    if min_vol >= 0:
+        ans[i][min_vol] = 1
+        s = min_vol
+    if max_vol <= m:
+        ans[i][max_vol] = 1
+        s = max_vol
+            
+
+print(ans)
--- 백준/탐색/[13565] 침투.py
+++ 백준/탐색/[13565] 침투.py
@@ -3,35 +3,29 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**9)
 m, n = map(int, input().split())
 
-
 def dfs(x, y):
-    # visited[x][y] = True
-    graph[x][y] = '2'
     for i in range(4):
         nx = dx[i] + x
         ny = dy[i] + y
 
         if 0<=nx<m and 0<=ny<n and graph[nx][ny] == '0':
-            dfs(nx,ny)
-            
-
-
+            graph[nx][ny] = '2'
+            dfs(nx, ny)
 
 graph = []
-# visited = [[False] * n for _ in range(m)]
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
 
 for i in range(m):
     graph.append(list(input().rstrip()))
 
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
 for i in range(n):
-    if graph[0][i] == '0': # outer line에서 0인 경우만 출발.
+    if graph[0][i] == '0':
+        graph[0][i] = '2'
         dfs(0, i)
 
-print(graph)
 if '2' in graph[-1]:
-    print('YES')
+    print("YES")
 else:
-    print('NO')
-
+    print("NO")

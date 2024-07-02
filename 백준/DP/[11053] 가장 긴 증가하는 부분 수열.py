e0a75552af76f48f31a6d81f0f9148090b786d48
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-07-02 23:20:52 +0900

    2024-07-02 LIS 복습

--- 백준/DP/[11053] 가장 긴 증가하는 부분 수열.py
+++ 백준/DP/[11053] 가장 긴 증가하는 부분 수열.py
@@ -1,12 +1,17 @@
-# https://thingjin.tistory.com/entry/%EB%B0%B1%EC%A4%80-11053%EB%B2%88-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-arr = list(map(int, input().split()))
-result = [1 for _ in range(n)]
 
+num_lst = list(map(int,input().split()))
+
+dp = [1] * n
+dp[0] = num_lst[0]
 for i in range(1, n):
-    for j in range(0, i):
-        if arr[i] > arr[j]:
-            result[i] = max(result[j]+1, result[i])
-    
-
-print(max(result))
+    for j in range(i):
+        if num_lst[i] > num_lst[j]:
+            dp[i] = max(dp[i], num_lst[i] + dp[j])
+        else:
+            dp[i] = max(dp[i], num_lst[i])
+            
+print(max(dp))

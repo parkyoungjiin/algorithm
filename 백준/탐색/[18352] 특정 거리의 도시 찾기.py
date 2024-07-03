8640d0e1aec187addab003dd858559c966af73fd
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-07-03 23:45:07 +0900

    2024-07-03 알고리즘

--- 백준/탐색/[18352] 특정 거리의 도시 찾기.py
+++ 백준/탐색/[18352] 특정 거리의 도시 찾기.py
@@ -1,36 +1,45 @@
-# https://www.acmicpc.net/problem/18352
-from collections import deque
 import sys
-f = sys.stdin.readline
+from collections import deque
+input = sys.stdin.readline
 
-n, m, k, x = map(int, f().split())
+n, m, k, x = map(int, input().split())
+
 graph = [[] for _ in range(n+1)]
-distance = [0] * (n+1)
-visited = [False] * (n+1)
 
 for _ in range(m):
-    a, b = map(int, f().split())
+    a, b = map(int, input().split())
+    # 단방향이기에 한 쪽에만 넣어준다.
     graph[a].append(b)
 
-def bfs(start):
-    answer = []
-    q = deque([start])
-    visited[start] = True
-    distance[start] = 0
-    while q:
-        now = q.popleft()
-        for i in graph[now]:
-            if not visited[i]:
-                visited[i] = True
-                q.append(i)
-                distance[i] = distance[now] + 1
-                if distance[i] == k:
-                    answer.append(i)
-    if len(answer) == 0:
-        print(-1)
-    else:
-        answer.sort()
-        for i in answer:
-            print(i, end='\n')
+distance = [-1] * (n+1) # ans -> 최단거리저장 배열
+distance[x] = 0
 
-bfs(x)
+q = deque([x]) # 시작점 
+while q:
+    current = q.popleft()
+    
+    for next_node in graph[current]:
+        if distance[next_node] == -1: # 방문하지 않은 경우
+            distance[next_node] = distance[current] + 1
+            q.append(next_node)       
+
+if k in distance:
+    for a in range(len(distance)):
+        if distance[a] == k:
+            print(a)
+else:
+    print(-1)
+
+
+
+    
+    
+        
+
+
+
+
+    
+
+
+

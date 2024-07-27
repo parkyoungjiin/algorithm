ff626601c1bc5d23d5cd626d9ec60c1ad49c63a1
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-07-27 17:23:00 +0900

    2024-07-27 알고리즘
    그리디 문제 풀이 완료(포스팅도 완료)

--- 백준/그리디/[1931] 회의실 배정.py
+++ 백준/그리디/[1931] 회의실 배정.py
@@ -4,11 +4,24 @@ input = sys.stdin.readline
 n = int(input())
 
 meetting = []
+
 for _ in range(n):
     meetting.append(list(map(int, input().split())))
 
-meetting = sorted(meetting)
+# 종료점 기준으로 정렬 -> 같을 때는 시작 시간이 빠른 순으로 정렬
+meetting.sort(key=lambda x:(x[1], x[0]))
 
-s, e = meetting[0][0], meetting[n-1][1]
+answer = 1 # 회의 최대 개수
+s, e = meetting[0][0], meetting[0][1]
+for i in range(1, n):
+    start = meetting[i][0]
+    if e <= start:
+        answer += 1
+        s, e = meetting[i][0], meetting[i][1]
 
-print(s, e)
+
+print(answer)
+
+
+
+

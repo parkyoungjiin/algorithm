4e2d4e7afe3a47e89f5a8f6c12f04bf2d2de1f29
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-07-28 23:45:57 +0900

    2024-07-28 알고리즘
    1번만에 성공하였음.

--- 백준/탐색/[2667] 단지번호붙이기.py
+++ 백준/탐색/[2667] 단지번호붙이기.py
@@ -1,50 +1,44 @@
-from collections import deque
-# 지도 크기
-N = int(input())
+import sys
 
-# 지도 정보
-map_info = []
-for _ in range(N):
-    map_info.append(list(map(int,input())))
+input = sys.stdin.readline
 
-# print(map_info)
-# 좌표 (상하좌우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+n = int(input())
 
-# bfs 안에서 단지 내 개수 출력.
-def bfs(map_info, a, b):
-    n = len(map_info)
-    queue = deque()
-    queue.append((a, b))
-    map_info[a][b] = 0
-    count = 1
-    
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                continue
-            if map_info[nx][ny] == 1:
-                map_info[nx][ny] = 0
-                queue.append(nx, ny)
-                count += 1
-    return count
-            
-# 단지 개수
-cnt = []
-# 단지 내 집 개수
-# 2차원 행렬
-for i in N:
-    for j in N:
-        # 사는 집인 경우
-        if map_info[i][j] == 1:
-            cnt.append(bfs(map_info, i, j))
+house = []
 
-    
-cnt.sort()
-print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
+for i in range(n):
+    house.append(list(map(int,input().rstrip())))
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+house_cnt = 0
+house_cnt_arr = []
+
+def dfs(x, y):
+    global house_cnt
+    global temp_house_cnt
+
+    house[x][y] = 0
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0<=nx<n and 0<=ny<n and house[nx][ny] == 1:
+            temp_house_cnt += 1
+            dfs(nx, ny)
+
+for i in range(n):
+    for j in range(n):
+        if house[i][j] == 1:
+            temp_house_cnt = 1
+            dfs(i,j)
+            house_cnt += 1
+            house_cnt_arr.append(temp_house_cnt)
+
+print(house_cnt)
+house_cnt_arr.sort()
+for i in house_cnt_arr:
+    print(i)
+

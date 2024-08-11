ab5dd0d3809faa1c9cc000879326f67d88e8da4b
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2024-08-11 23:04:03 +0900

    2024-08-11 알고리즘

--- 백준/재귀(백트래킹)/[1074] Z.py
+++ 백준/재귀(백트래킹)/[1074] Z.py
@@ -1,39 +1,34 @@
 import sys
-input = sys.stdin.readline
+sys.setrecursionlimit(10**3)
 
-n, r, c = map(int, input().split())
-# 방문여부 저장 -> 시간 초과 발생(틀림)
-# arr = [[0]*(2**n) for _ in range(2**n)]
+N, r, c = map(int,input().split())
+size = 2**N
 
-# 분할 탐색
-answer = 0
-while n != 0:
-    # 4등분을 위해 -1 해줌.
-    n -= 1
-    size = 2**n
+def dfs(row, col, n, value):
+    n = n // 2
 
-    # 1사분면
-    if r < size and c < size:
-        pass
+    if row < n and col < n:
+        if n == 1:
+            print(value)
+            exit(0)
+        dfs(row, col, n, value)
 
-    # 2사분면
-    elif r < size and c >= size:4
-        # size * size -> 분할된 사분면의 총 원소(셀)의 수 계산.
-        answer += size * size
-        c -= size
-    
-    # 3사분면(행은 4등분 한 값보다 크고, 열은 4등분한 값보다 작은 경우)
-    elif r >= size and c < size:
-        answer+=size * size * 2 #1, 2사분면만큼 pass
-        r-=size
+    elif row < n and col >= n:
+        if n == 1:
+            print(value + 1)
+            exit(0)
+        dfs(row, col - n, n, value + n ** 2)
 
-    # 좌표가 4사분면에 속할 때
-    else:
-        answer+=size * size * 3 #1, 2, 3사분면만큼 pass
-        r-=size
-        c-=size
-    
+    elif row >= n and col < n:
+        if n == 1:
+            print(value + 2)
+            exit(0)
+        dfs(row - n, col, n, value + n ** 2 * 2)
 
-print(answer)
+    elif row >= n and col >= n:
+        if n == 1:
+            print(value + 3)
+            exit(0)
+        dfs(row - n, col - n, n, value + n ** 2 * 3)
 
-
+dfs(r,c,size,0)
